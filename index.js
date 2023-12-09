1b6ef0b58001f51e3df9a1d3f0f387d76788fa62
Apple <jonathanhyefur@gmail.com>
2023-12-09 19:39:11 +0100

    new changes

---
+++ index.js
@@ -0,0 +1,60 @@
+// const WebSocket = require('ws');
+// const wss = new WebSocket.Server({ port: 8080 });
+const express = require('express');
+const expressWs = require('express-ws');
+const app = express();
+
+let lightSwitch;
+let doorMode;
+let surveillanceMode;
+
+expressWs(app);
+
+const port = 8000;
+const wsClients = [];
+const espClient = [];
+
+
+app.ws('/esp', (ws) => {
+console.log('ESP32 connected');
+espClient.push(ws);
+               
+ws.on('message', (message) => {
+ console.log(`Received from ESP32: ${message}`);
+
+ // Forward the message to all Flutter clients
+   wsClients.forEach((flutterSocket) => {
+   flutterSocket.send(message);
+     });
+   });
+
+
+});
+
+app.ws('/flutter', (ws) => {
+  console.log('Flutter app connected');
+  wsClients.push(ws);
+
+  ws.on('message', (newMessage) => {
+
+    console.log(`Received from Flutter app: ${newMessage}`);
+    const message = newMessage.toString();
+
+    // Forward the message to all Flutter clients
+    espClient.forEach((espSocket) => {
+      espSocket.send(message);
+        });
+
+  });
+
+
+  ws.on('close', () => {
+    console.log('Flutter app disconnected');
+    wsClients.splice(wsClients.indexOf(ws), 1);
+  });
+
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
