85462268dd6d0c4ab4c43e987fdeb1957f57c7d7
Apple <jonathanhyefur@gmail.com>
2023-12-10 10:43:08 +0100

    response message from server when uploaded

--- index.js
+++ index.js
@@ -9,49 +9,45 @@ const wsClients = [];
 const espClient = [];
 
 
+//ESP32 MODULE CONNECTION TO THE WEB SERVICE
 app.ws('/esp', (ws) => {
 console.log('ESP32 connected');
 espClient.push(ws);
                
 ws.on('message', (message) => {
  console.log(`Received from ESP32: ${message}`);
-
- // Forward the message to all Flutter clients
+ // Forward the message to all Mobile clients
    wsClients.forEach((flutterSocket) => {
    flutterSocket.send(message);
      });
    });
-
-
 });
+//ESP32 MODULE CONNECTION TO THE WEB SERVICE
 
+
+
+//Mobile Application Connection to the web server
 app.ws('/flutter', (ws) => {
-  
   console.log('Flutter app connected');
   wsClients.push(ws);
 
-  ws.on('message', (newMessage) => {
+  wsClients.forEach((flutterSocket) => {
+    flutterSocket.send(`Connected`)
+      });
 
+  ws.on('message', (newMessage) => {
     console.log(`Received from Flutter app: ${newMessage}`);
     const message = newMessage.toString();
-
-    wsClients.forEach((flutterSocket) => {
-      flutterSocket.send(`Connected`)
-        });
-
     espClient.forEach((espSocket) => {
       espSocket.send(message);
         });
-
   });
-
-
   ws.on('close', () => {
     console.log('Flutter app disconnected');
     wsClients.splice(wsClients.indexOf(ws), 1);
   });
-
 });
+//Mobile Application Connection to the web server
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
